94e78c57dfbf5759198f717b40541f2ec84a48b3
Sabin Dang <sdang@Aurora.local>
2008-11-25 07:28:30 -0800

    fixed sessionS migration typo

--- db/migrate/001_add_sessions.rb
+++ db/migrate/001_add_sessions.rb
@@ -1,4 +1,4 @@
-class AddSessions < SessionMigration
+class AddSessions < SessionsMigration
   def self.up
     create_table :sessions do |t|
       t.column :session_id, :string, :references => nil
